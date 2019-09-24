65288ebbf6a58d8a9ca2e4c325e9d60c97430358
Bruno Alves <brunosalves3@gmail.com>
2019-09-24 14:26:23 -0300

    Updating nifi.properties

--- env/nifi/scripts/helpers.sh
+++ env/nifi/scripts/helpers.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-case $1 in
-'init') init;;
-'props') apply_properties;;
-*) echo "Opcao Invalida!" ;;
-esac
-
-init() {
+main() {
     apply_properties
 }
 
 apply_properties() {
     cat ${NIFI_HOME}/conf/custom/nifi.properties > ${NIFI_HOME}/conf/nifi.properties
 }
+
+case $1 in
+    'init') main;;
+    'props') apply_properties;;
+    *) echo "Opcao Invalida!";;
+esac
